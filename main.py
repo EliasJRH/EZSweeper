c478436bf11dc0a55c8b7973a9e625449e435d11
Elias Hawa <elias.hawa2468@gmail.com>
2022-03-19 21:33:15 -0400

    Get colors

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import webbrowser
 #block width: 25px
 #block height: 25px
 
+# Grass pixel 1 = (162, 209, 73)
+# Grass pixel 2 = (185, 221, 119)
+
+COLORS = {
+    (25, 118, 210): 1,
+    (56, 142, 60): 2,
+    (211, 47, 47): 3,
+    (123, 31, 162): 4,
+    (255, 143, 0): 5,
+    (0, 151, 167): 6,
+    (66, 66, 66): 7,
+    (160, 155, 152): 8
+}
+
 def open_minesweeper():
     webbrowser.open("https://www.google.com/search?client=firefox-b-d&q=minesweeper")
     time.sleep(2)
@@ -23,12 +37,36 @@ def start_game():
     pag.moveTo(665, 365)
 
 def process():
+    pag.PAUSE = 5
+    # pag.PAUSE = 0.001
+    colors = set()
+
     # TODO:
     # Have to analyze each number box and find where the numbers intersect at the same position in the box
     # Use that position as a way of grabbing the color and identify the number of adjacent bombs by that color
-    x, y = pag.position()
+    
+    while True:
+        x = 655
+        y = 365
+        for yy in range(20):
+            for xx in range(23):
+                x += 25
+                pag.moveTo(x, y)
+                for z in range(15):
+                    cur = pag.screenshot()
+                    print(cur.getpixel((x, y)))
+                    pag.move(1, 0)
+            y += 25
+            x = 655
+
+    # while True:
+    #     time.sleep(1)
+    #     x,y = pag.position()
+    #     cur = pag.screenshot()
+    #     print(cur.getpixel((x,y)))
 
 def main():
+    
     open_minesweeper()
     select_difficulty()
     start_game()
