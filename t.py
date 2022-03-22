33f648cee94bef2cef13f7e4cd96f485cb5beeee
Elias Hawa <elias.hawa2468@gmail.com>
2022-03-22 10:26:02 -0400

    create prototype for properly getting colors from tile

---
+++ t.py
@@ -0,0 +1,13 @@
+import pyautogui
+import consts
+
+#664, 370
+
+while True:
+    print(pyautogui.position())
+    x = int(input())
+    pyautogui.moveTo(x, 370)
+    x, y = pyautogui.position()
+    sc = pyautogui.screenshot(region=(x - 12.5, y - 12.5, 25, 23))
+    sc.save("1.png")
+    print(sc.getcolors())
