c10f48a1a145c4c45fd5be7eb35639d5cb82f08b
Elias Hawa <elias.hawa2468@gmail.com>
2022-04-03 08:34:00 -0400

    Add flag and ignore function

--- funcs/actions.py
+++ funcs/actions.py
@@ -1,7 +1,7 @@
 import pyautogui as pag
 from funcs.utils import is_valid_coordinate, is_valid_coordinate_no_adds
 from funcs.tile_identification import get_tile
-from consts import TO_SEARCH
+from consts import START_X_MP, START_Y_MP, TO_SEARCH
 
 # Flags a single tile of a given x and y coordinate
 def flag_tile(x, y):
@@ -17,16 +17,31 @@ def click_tile(x, y):
         pag.click(button="left")
 
 
+# # Flags all adjacent tiles
+# def flag_adj_tiles(x, y):
+#     flagged = False
+#     for c in TO_SEARCH:
+#         if is_valid_coordinate(x, y, c[0], c[1]):
+#             tile = get_tile(x + c[0], y + c[1])
+#             if tile == "grass":
+#                 flagged = True
+#                 pag.click(button="right")
+#     return flagged
+
+
 # Flags all adjacent tiles
-def flag_adj_tiles(x, y):
-    clicked = False
+def flag_and_ignore_adj_tiles(x, y, ignore):
+    flagged = False
     for c in TO_SEARCH:
         if is_valid_coordinate(x, y, c[0], c[1]):
             tile = get_tile(x + c[0], y + c[1])
             if tile == "grass":
-                clicked = True
+                flagged = True
+                ignore.add(
+                    (((x + c[0] - START_X_MP) // 25), ((y + c[1] - START_Y_MP) // 25))
+                )
                 pag.click(button="right")
-    return clicked
+    return flagged
 
 
 # Clicks all adjacent tiles
