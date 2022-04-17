5b7dc3e93147bf2bc5edf7df73b0f0c413cc18a4
Elias Hawa <elias.hawa2468@gmail.com>
2022-04-16 23:11:07 -0400

    FIx if

--- main.py
+++ main.py
@@ -75,33 +75,33 @@ def play():
                     ignore.add((x_c, y_c))
                 elif tile in NUMBERS:
                     adj_tiles = count_adj_tiles(x_mp, y_mp)
+                    cur_change_made = False
                     if adj_tiles[1] == tile:
                         ignore.add((x_c, y_c))
                         if click_adj_tiles(x_mp, y_mp):
                             change_made = True
-                            for adj_c in ADJ_C:
-                                # fmt: off
-                                if ((x_c + adj_c[0], y_c + adj_c[1]) not in ignore # we're not ignoring the adjacent coordinate
-                                    and is_valid_mouse_pos(x_mp + (adj_c[0] * 25), y_mp + (adj_c[1] * 25)) # it's a valid coordinate
-                                    and (x_c + adj_c[0], y_c + adj_c[1]) not in new_t_set): # we're not already going to search it
-                                        NEW_T.append([x_c + adj_c[0], y_c + adj_c[1]])
-                                        new_t_set.add((x_c + adj_c[0],y_c + adj_c[1]))
-                                # fmt: on
+                            cur_change_made = True
+                            
                     elif adj_tiles[0] + adj_tiles[1] == tile:
                         ignore.add((x_c, y_c))
                         if flag_and_ignore_adj_tiles(x_mp, y_mp, ignore):
                             change_made = True
-                            for adj_c in ADJ_C:
-                                # fmt: off
-                                if ((x_c + adj_c[0], y_c + adj_c[1]) not in ignore
-                                    and is_valid_mouse_pos(x_mp + (adj_c[0] * 25), y_mp + (adj_c[1] * 25))
-                                    and (x_c + adj_c[0],y_c + adj_c[1]) not in new_t_set):
-                                        NEW_T.append([x_c + adj_c[0], y_c + adj_c[1]])
-                                        new_t_set.add((x_c + adj_c[0],y_c + adj_c[1]))
-                                # fmt: on
+                            cur_change_made = True
+                            
                     elif adv_search:
                         if advanced_search_tile(x_mp, y_mp, tile):
                             change_made = True
+                            cur_change_made = True
+
+                    if cur_change_made:
+                        for adj_c in ADJ_C:
+                            # fmt: off
+                            if ((x_c + adj_c[0], y_c + adj_c[1]) not in ignore # we're not ignoring the adjacent coordinate
+                                and is_valid_mouse_pos(x_mp + (adj_c[0] * 25), y_mp + (adj_c[1] * 25)) # it's a valid coordinate
+                                and (x_c + adj_c[0], y_c + adj_c[1]) not in new_t_set): # we're not already going to search it
+                                    NEW_T.append([x_c + adj_c[0], y_c + adj_c[1]])
+                                    new_t_set.add((x_c + adj_c[0],y_c + adj_c[1]))
+                            # fmt: on
             ind += 1
             if keyboard.is_pressed("q"):
                 quit()
