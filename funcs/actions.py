27a6511341ffecf97858743b40e0fa20b8c0a055
Elias Hawa <elias.hawa2468@gmail.com>
2022-05-03 12:46:02 -0400

    Add difficulties to flag and ignore

--- funcs/actions.py
+++ funcs/actions.py
@@ -2,10 +2,13 @@ import sys
 import pyautogui as pag
 from funcs.utils import is_valid_mouse_pos
 from funcs.tile_identification import get_tile
-from consts.mouse_positions import START_X_MP, START_Y_MP
-from consts.other import TO_SEARCH, TILE_WIDTH_EASY, TILE_WIDTH_MED, TILE_WIDTH_HARD, ADJ_COORDS
+from consts.mouse_positions import START_X_MP_EASY, START_Y_MP_EASY, START_X_MP_MED, START_Y_MP_MED, START_X_MP_HARD, START_Y_MP_HARD
+from consts.other import TILE_WIDTH_EASY, TILE_WIDTH_MED, TILE_WIDTH_HARD, ADJ_COORDS
 
 TILE_WIDTH = TILE_WIDTH_HARD
+START_X_MP = START_X_MP_HARD
+START_Y_MP = START_Y_MP_HARD
+
 
 # Flags a single tile of a given x and y coordinate
 def flag_tile(x_mp, y_mp, bombs):
@@ -28,8 +31,9 @@ def click_tile(x_mp, y_mp):
 
 # Flags and ignores all adjacent tiles
 # Returns true if a tile was flagged, false otherwise
-def flag_and_ignore_adj_tiles(x_mp, y_mp, ignore, bombs):
+def flag_and_ignore_adj_tiles(x_mp, y_mp, ignore):
     flagged = False
+    bombs_flagged = 0
     for c in ADJ_COORDS:
         if is_valid_mouse_pos(x_mp + (c[0] * TILE_WIDTH), y_mp + (c[1] * TILE_WIDTH)):
             tile = get_tile(x_mp + (c[0] * TILE_WIDTH), y_mp + (c[1] * TILE_WIDTH))
@@ -41,10 +45,11 @@ def flag_and_ignore_adj_tiles(x_mp, y_mp, ignore, bombs):
                         ((y_mp + (c[1] * TILE_WIDTH) - START_Y_MP) // TILE_WIDTH),
                     )
                 )
+                print(f"ignoring {((x_mp + (c[0] * TILE_WIDTH) - START_X_MP) // TILE_WIDTH)}, {((y_mp + (c[1] * TILE_WIDTH) - START_Y_MP) // TILE_WIDTH)} because flag two from {((x_mp - START_X_MP) // TILE_WIDTH)}, {((y_mp - START_Y_MP) // TILE_WIDTH)}")
                 pag.moveTo(x_mp + (c[0] * TILE_WIDTH), y_mp + (c[1] * TILE_WIDTH))
                 pag.click(button="right")
-                bombs -= 1
-    return flagged
+                bombs_flagged += 1
+    return flagged, bombs_flagged
 
 
 # Clicks all adjacent tiles
@@ -66,7 +71,13 @@ for x in range(1, len(sys.argv)):
     if cur_arg[0] == "-d":
         if cur_arg[1].lower() == "easy":
             TILE_WIDTH = TILE_WIDTH_EASY
+            START_X_MP = START_X_MP_EASY
+            START_Y_MP = START_Y_MP_EASY
         elif cur_arg[1].lower() == "medium":
             TILE_WIDTH = TILE_WIDTH_MED
+            START_X_MP = START_X_MP_MED
+            START_Y_MP = START_Y_MP_MED
         else:
             TILE_WIDTH = TILE_WIDTH_HARD
+            START_X_MP = START_X_MP_HARD
+            START_Y_MP = START_Y_MP_HARD
