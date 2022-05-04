e19ffec93a1c9cb3acec1a7d1b1ed87dbda5704e
Elias Hawa <elias.hawa2468@gmail.com>
2022-05-04 10:10:34 -0400

    Remove to search from consts

--- consts/other.py
+++ consts/other.py
@@ -5,16 +5,5 @@ TILE_WIDTH_HARD = 25
 # Numbers
 NUMBERS = set({1, 2, 3, 4, 5, 6, 7, 8, 9})
 
-# Search coordinates
-TO_SEARCH = [
-    [-25, -25],
-    [0, -25],
-    [25, -25],
-    [-25, 0],
-    [25, 0],
-    [-25, 25],
-    [0, 25],
-    [25, 25],
-]
-
+# Adjacent search coordinates
 ADJ_COORDS = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
