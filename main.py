8e1aa0ce5d4cafa9c47da96f5be68e97119f4ba6
Elias Hawa <elias.hawa2468@gmail.com>
2022-05-29 17:36:44 -0400

    Refactor flag function and add some comments

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def play():
     pag.PAUSE = 0
 
     bombs = 99
-    grass_length = 24
+    board_length = 24
     START_X_MP = START_X_MP_HARD
     START_Y_MP = START_Y_MP_HARD
     TILE_WIDTH = TILE_WIDTH_HARD
@@ -26,19 +26,19 @@ def play():
         if cur_arg[0] == "-d":
             if cur_arg[1].lower() == "easy":
                 bombs = 10
-                grass_length = 10
+                board_length = 10
                 START_X_MP = START_X_MP_EASY
                 START_Y_MP = START_Y_MP_EASY
                 TILE_WIDTH = TILE_WIDTH_EASY
             elif cur_arg[1].lower() == "medium":
                 bombs = 40
-                grass_length = 18
+                board_length = 18
                 START_X_MP = START_X_MP_MED
                 START_Y_MP = START_Y_MP_MED
                 TILE_WIDTH = TILE_WIDTH_MED
             else:
                 bombs = 99
-                grass_length = 24
+                board_length = 24
                 START_X_MP = START_X_MP_HARD
                 START_Y_MP = START_Y_MP_HARD
                 TILE_WIDTH = TILE_WIDTH_HARD
@@ -49,7 +49,7 @@ def play():
     T = copy.deepcopy(TILE_COORDS)
 
     # fmt: off
-    # Flag that determines whether or not we should check the entire board
+    # Flag that determines whether or not the bot should check the entire board
     change_made = True
     
     # Flag that determines whether or not advanced searching should be used
@@ -70,13 +70,18 @@ def play():
         end = False
         ind = 0
 
+        # If a change wasn't made in the last board sweep, set a flag that on the next board sweep if no change is made, perform advanced searching
         if not change_made and not adv_search_flag:
             adv_search_flag = True
             T = copy.deepcopy(TILE_COORDS)
+
+        # If a change wasn't made in the last board sweep and the advanced searched flag has been set, run an advanced search along with the regular searches
         elif not change_made and adv_search_flag:
             adv_search = True
             adv_search_flag = False
             T = copy.deepcopy(TILE_COORDS)
+
+        # Otherwise, search as normal
         else:
             adv_search = False
             adv_search_flag = False
@@ -89,23 +94,28 @@ def play():
         new_t_set.clear()
         adv_search_pairs.clear()
 
+        # Boolean representing if a change has been made anywhere on the board on this sweep
         change_made = False
 
+        # grass_count counts the number of grass enountered in a single row
+        # first_non_grass_full_sweep is a boolean repsenting whether the bot has seen a non grass tile in the current sweep
         grass_count = 0
-        first_non_grass = False
+        first_non_grass_in_full_sweep = False
 
         while ind < len(T):
             coords = T[ind]
             x_c, y_c = coords[0], coords[1]
 
-            if grass_count == grass_length and first_non_grass:
+            # If the bot has seen a non grass tile and the bot has encountered a full row of grass,
+            # then the bot can automatically break out of the current sweep and start with the new tiles
+            # because there will be nothing more to search in the current sweep
+            if grass_count == board_length and first_non_grass_in_full_sweep:
                 break
             elif x_c == 0:
                 grass_count = 0
 
             print(
-                f"\r{x_c}, {y_c}",
-                f"bombs: {bombs}",
+                f"\r{x_c}, {y_c}" "bombs: {bombs}",
                 len(TILE_COORDS),
                 len(ignore),
                 end=" ",
@@ -124,24 +134,23 @@ def play():
                     if bombs == 0:
                         click_tile(x_mp, y_mp)
                 elif tile in NUMBERS:
-                    first_non_grass = True
+
+                    first_non_grass_in_full_sweep = True
                     adj_tiles = count_adj_tiles(x_mp, y_mp)
-                    cur_change_made = False
+                    change_made_on_cur_tile = False
+
                     if adj_tiles[1] == tile:
                         ignore.add((x_c, y_c))
                         if click_adj_tiles(x_mp, y_mp):
                             change_made = True
-                            cur_change_made = True
+                            change_made_on_cur_tile = True
 
                     elif adj_tiles[0] + adj_tiles[1] == tile:
                         ignore.add((x_c, y_c))
-                        flagged, bombs_flagged = flag_and_ignore_adj_tiles(
-                            x_mp, y_mp, ignore
-                        )
-                        if flagged:
+                        bombs_flagged = flag_and_ignore_adj_tiles(x_mp, y_mp, ignore)
+                        if bombs_flagged:
                             change_made = True
-                            cur_change_made = True
-                        if bombs_flagged > 0:
+                            change_made_on_cur_tile = True
                             bombs -= bombs_flagged
 
                     elif adv_search:
@@ -150,11 +159,11 @@ def play():
                         # fmt: on
                         if change_made_adv:
                             change_made = True
-                            cur_change_made = True
+                            change_made_on_cur_tile = True
                         if bombs_flagged > 0:
                             bombs -= bombs_flagged
 
-                    if cur_change_made:
+                    if change_made_on_cur_tile:
                         for adj_c in ADJ_COORDS:
                             # fmt: off
                             if ((x_c + adj_c[0], y_c + adj_c[1]) not in ignore # we're not ignoring the adjacent coordinate
